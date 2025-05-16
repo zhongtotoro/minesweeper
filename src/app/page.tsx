ed815a97e897a43dd2a1f86a37bd4200199f517e
zhongtotoro <s1F102400055@iniad.org>
2025-05-17 00:38:29 +0900

    'マインスイーパー'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,59 +1,39 @@
 'use client';
 
+import { useState } from 'react';
 import styles from './page.module.css';
-
+const calcTotalPoint = (arr: number[]) => {//userI,bombがくる
+  return for(let i = 0; i<arr.length);
+};
 export default function Home() {
+  const [samplePoints, setSamplePoints] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  console.log(samplePoints);
+  const [sampleCounter, setSampleCounter] = useState(0);
+  console.log(sampleCounter);
+  const clickHundler = () => {
+    const newSamplePoint = structuredClone(samplePoints);
+    newSamplePoint[sampleCounter] += 1;
+    setSamplePoints(newSamplePoint);
+    setSampleCounter((sampleCounter + 1) % 14);
+  };
+  const totalPoint = calcTotalPoint(samplePoints);
+  console.log(totalPoint);
   return (
     <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code} style={{ backgroundColor: '#fafafa' }}>
-            src/app/page.tsx
-          </code>
-        </p>
-
-        <div className={styles.grid}>
-          <a className={styles.card} href="https://nextjs.org/docs">
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a className={styles.card} href="https://nextjs.org/learn">
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a className={styles.card} href="https://github.com/vercel/next.js/tree/master/examples">
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            className={styles.card}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <img src="vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <div
+        className={styles.samplecell}
+        style={{ backgroundPosition: `${sampleCounter * -30}px` }}
+      />
+      <button onClick={clickHundler}>Click</button>
     </div>
   );
 }
+
+//    再起関数 ｆｎ（）
+//joutai userInputs[0-4]右クリック左クリック？ユーザーがどのマスで『なにしたか
+//joutai bombmap[0,1]爆弾あるかどうか
+//二つ合わせてboardにしてｔｓｘに送る
+//shokyu-jokyu + custom（状態必要）
+//見た目一緒（数字以外）バーいらない、数字フォント簡単なのでＯＫ
+//時間 useEffect(状態)
+//初回で爆発しない→クリックした後にbombマップを作ればいい
