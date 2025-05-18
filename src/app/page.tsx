8982a8e2fce96a4c5799a144bd8f13652829d84b
zhongtotoro <s1F102400055@iniad.org>
2025-05-18 22:35:42 +0900

    'セルも置けない'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,32 @@
 
 import { useState } from 'react';
 import styles from './page.module.css';
-const calcTotalPoint = (arr: number[]) => {//userI,bombがくる
-  return for(let i = 0; i<arr.length);
+
+let sum: number = 0;
+const calcTotalPoint = (arr: number[]) => {
+  //userI,bombがくる
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    return sum;
+  } //returnの後ろは数字（？）式は前に、
 };
 export default function Home() {
+  const [bombMap, setBombMap] = useState([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+  const [userInputs, setUserInputs] = useState([0]);
+  const [board, setBoard] = useState([bombMap, userInputs]);
+  const clickHundler = (x: number, y: number) => {
+    const newBoard = structuredClone(board);
+  };
   const [samplePoints, setSamplePoints] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
   console.log(samplePoints);
   const [sampleCounter, setSampleCounter] = useState(0);
@@ -20,10 +42,13 @@ export default function Home() {
   console.log(totalPoint);
   return (
     <div className={styles.container}>
-      <div
-        className={styles.samplecell}
-        style={{ backgroundPosition: `${sampleCounter * -30}px` }}
-      />
+      <div className={styles.board}>
+        {board.map(row.y)}
+        <div
+          className={styles.samplecell}
+          style={{ backgroundPosition: `${sampleCounter * -30}px` }}
+        />
+      </div>
       <button onClick={clickHundler}>Click</button>
     </div>
   );
@@ -37,3 +62,8 @@ export default function Home() {
 //見た目一緒（数字以外）バーいらない、数字フォント簡単なのでＯＫ
 //時間 useEffect(状態)
 //初回で爆発しない→クリックした後にbombマップを作ればいい
+
+//計算値 状態＋計算
+
+//ユーザーがクリック→board[useInput][bompmap]
+//八方向を調べて（矢印で表せる）爆弾（samplecounter）があったら＋１でー３０Px分動かす
